384409b02fc40260e3338f7b4ccc9bb01684fdd0
Sergei2000 <dsdem@mail.ru>
2020-03-22 13:09:21 +0300

    fourth commit

--- sources/source.cpp
+++ sources/source.cpp
@@ -10,16 +10,16 @@ typedef boost::shared_ptr <boost::asio::ip::tcp::socket> socket_ptr;
 
 void clear_buf(char *buf, int size) {
     for (int i = 0; i < size; ++i) {
-        buf[i] = '/0';
+        buf[i] = 0;
     }
 }
-boost::asio::ip::tcp::io_service service;
+boost::asio::io_service service;
 std::string receive_message(socket_ptr &sock) {
     char buf[500];
     clear_buf(buf, buf_size);
     int length;
-    sock->read_some(buffer(&length, 4));
-    sock->read_some(buffer(buf, length));
+    sock->read_some(boost::asio::buffer(&length, 4));
+    sock->read_some(boost::asio::buffer(buf, length));
     return std::string(buf, length);
 }
 
@@ -31,7 +31,8 @@ void send_string(socket_ptr &sock, std::string &msg) {
 
 void sync_echo(std::string &msg, int port_num) {
     socket_ptr sock(new boost::asio::ip::tcp::socket(service));
-    boost::asio::ip::tcp::endpoint ep(ip::address::from_string("192.168.1.2"),
+    boost::asio::ip::tcp::endpoint ep(boost::asio::ip::address::from_string
+    ("192.168.1.2"),
     std::move(port_num));
     sock->connect(ep);
     char buff[200];
@@ -60,7 +61,7 @@ void sync_echo(std::string &msg, int port_num) {
     }
 }
 
-int main(int argc, char *argv[]) {
+int main() {
     std::string msg = "hello\n";
     std::string msg2 = "hi\n";
     boost::thread first(sync_echo, msg, 8001);
