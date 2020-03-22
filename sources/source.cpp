035e935fee7e332b90e1516698869825d2005277
Sergei2000 <dsdem@mail.ru>
2020-03-22 12:28:07 +0300

    first commit

--- sources/source.cpp
+++ sources/source.cpp
@@ -6,80 +6,72 @@
 #include <boost/thread/thread.hpp>
 
 using namespace boost::asio;
-static const int buf_size=500;
-typedef boost::shared_ptr<ip::tcp::socket> socket_ptr;
-void clear_buf(char * buf,int size)
-{
-    for (int i=0;i<size;++i)
-    {
-        buf[i]='/0';
+static const int buf_size = 500;
+typedef boost::shared_ptr <ip::tcp::socket> socket_ptr;
+
+void clear_buf(char *buf, int size) {
+    for (int i = 0; i < size; ++i) {
+        buf[i] = '/0';
     }
 }
+
 io_service service;
 
 
-std::string receive_message(socket_ptr& sock)
-{
+std::string receive_message(socket_ptr &sock) {
     char buf[buf_size];
-    clear_buf(buf,buf_size);
+    clear_buf(buf, buf_size);
     int length;
-    sock->read_some(buffer(&length,4));
-    sock->read_some(buffer(buf,length));
-    return std::string(buf,length);
+    sock->read_some(buffer(&length, 4));
+    sock->read_some(buffer(buf, length));
+    return std::string(buf, length);
 }
-void send_string(socket_ptr& sock,std::string& msg)
-{
-    int length=msg.size();
-    sock->write_some(boost::asio::buffer(&length,4));
-    sock->write_some(boost::asio::buffer(msg,length));
+
+void send_string(socket_ptr &sock, std::string &msg) {
+    int length = msg.size();
+    sock->write_some(boost::asio::buffer(&length, 4));
+    sock->write_some(boost::asio::buffer(msg, length));
 }
-void sync_echo(std::string& msg,int port_num)
-{
+
+void sync_echo(std::string &msg, int port_num) {
     socket_ptr sock(new ip::tcp::socket(service));
-    ip::tcp::endpoint ep( ip::address::from_string("192.168.1.2"), std::move(port_num));
+    ip::tcp::endpoint ep(ip::address::from_string("192.168.1.2"), std::move(port_num));
     sock->connect(ep);
     char buff[200];
-    int counter=0;
-    std::string data="client list\n",data1="ping\n";
-    bool check=true;
-    int c=msg.size();
-    sock->write_some(boost::asio::buffer(&c,4));
-    sock->write_some(boost::asio::buffer(msg,c));
-      while (true)
-    {
-          if (check)
-          {
-              int a=data1.size();
-              sock->write_some(boost::asio::buffer(&a,4));
-              sock->write_some(boost::asio::buffer(data1,a));
+    int counter = 0;
+    std::string data = "client list\n", data1 = "ping\n";
+    bool check = true;
+    int c = msg.size();
+    sock->write_some(boost::asio::buffer(&c, 4));
+    sock->write_some(boost::asio::buffer(msg, c));
+    while (true) {
+        if (check) {
+            int a = data1.size();
+            sock->write_some(boost::asio::buffer(&a, 4));
+            sock->write_some(boost::asio::buffer(data1, a));
 
-          }
+        } else {
+            int a = data.size();
+            sock->write_some(boost::asio::buffer(&a, 4));
+            sock->write_some(boost::asio::buffer(data, a));
 
-          else
-          {
-              int a=data.size();
-              sock->write_some(boost::asio::buffer(&a,4));
-              sock->write_some(boost::asio::buffer(data,a));
-
-          }
-          int b;
-          sock->read_some(boost::asio::buffer(&b,4));
-        sock->read_some(boost::asio::buffer(buff,b));
-        std::cout<<" "+std::string(buff)<<std::endl;
+        }
+        int b;
+        sock->read_some(boost::asio::buffer(&b, 4));
+        sock->read_some(boost::asio::buffer(buff, b));
+        std::cout << " " + std::string(buff) << std::endl;
         boost::this_thread::sleep(boost::posix_time::millisec(4));
-        check=(!check);
+        check = (!check);
     }
 
 
-
-
 }
-int main(int argc, char* argv[])
-{
-    std::string msg="hello\n";
-    std::string msg2="hi\n";
-    boost::thread first(sync_echo,msg,8001);
-    boost::thread second(sync_echo,msg2,8001);
+
+int main(int argc, char *argv[]) {
+    std::string msg = "hello\n";
+    std::string msg2 = "hi\n";
+    boost::thread first(sync_echo, msg, 8001);
+    boost::thread second(sync_echo, msg2, 8001);
     first.join();
     second.join();
     return 0;
